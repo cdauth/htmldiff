e160f6eb2aa485d0d080eed4686b88a0dfd589f3
Candid Dauth <cdauth@cdauth.eu>
2011-03-25 01:52:14 +0000

    Initial commit, checking in current working version

---
+++ htmldiff.php
@@ -0,0 +1,89 @@
+<?php
+	// The HTMLDiff module uses some MediWiki functions (which are defined in http://svn.wikimedia.org/viewvc/mediawiki/trunk/phase3/includes/GlobalFunctions.php?view=markup&pathrev=58267)
+	// This file creates dummy functions so that the HTMLDiff module works without them
+
+	if(!function_exists("wfProfileIn"))
+	{
+		function wfProfileIn($method)
+		{
+			// Debugging function, is called at the beginning of some methods
+		}
+	}
+
+	if(!function_exists("wfProfileOut"))
+	{
+		function wfProfileOut($method)
+		{
+			// Debugging function, is called at the end of some methods
+		}
+	}
+	
+	if(!function_exists("wfDebug"))
+	{
+		function wfDebug($msg)
+		{
+			// Debugging function
+		}
+	}
+	
+	if(!function_exists("wfMsg"))
+	{
+		function wfMsg($key)
+		{
+			// Looks up a localised message in MediaWiki
+			return $key;
+		}
+	}
+	
+	if(!function_exists("wfMsgExt"))
+	{
+		function wfMsgExt($key, $options)
+		{
+			// Looks up some localised message in MediaWiki
+			// The message for the key "diff-movedoutof" is "moved out of $1", for "diff-pre" it is "a preformatted block", for example
+			// This is used by the HTMLDiff module to describe the changes in words. As we don’t really use that, we just return
+			// some example data here.
+			return "wfMsgExt(".implode(", ", func_get_args()).")";
+		}
+	}
+
+	if(!function_exists("wfEmptyMsg"))
+	{
+		function wfEmptyMsg($msg, $wfMsgOut)
+		{
+			// $msg is $key for wfMsgExt, $wfMsgOut is wfMsgExt($key). Returns true when wfMsgExt could not a message with that
+			// key
+			return true;
+		}
+	}
+	
+	if(!function_exists("wfUrlProtocols"))
+	{
+		function wfUrlProtocols()
+		{
+			return "http:\\/\\/|https:\\/\\/|ftp:\\/\\/|irc:\\/\\/|gopher:\\/\\/|telnet:\\/\\/|nntp:\\/\\/|worldwind:\\/\\/|mailto:|news:|svn:\\/\\/|git:\\/\\/|mms:\\/\\/";
+		}
+	}
+	
+	$dir = dirname(__FILE__);
+	require_once($dir."/HTMLDiff.php");
+	require_once($dir."/Nodes.php");
+	require_once($dir."/Xml.php");
+	require_once($dir."/Sanitizer.php");
+	require_once($dir."/Diff.php");
+
+	/**
+	 * Return the difference between two HTML documents.
+	 * @param String $a The first HTML document
+	 * @param String $b The second HTML document
+	 * @return String An HTML document that represents the difference between the two HTML documents by assigning the class names
+	 *                diff-html-added and diff-html-removed to the changed elements.
+	*/
+
+	function html_diff($a, $b)
+	{
+		$out = new ChangeText();
+		$htmldiffer = new HTMLDiffer(new DelegatingContentHandler($out));
+		$htmldiffer->htmlDiff($a, $b);
+		return $out->toString();
+	}
